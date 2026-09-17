3631c85b3b590e1a171aecaf71e5ecd3ccd87738
Dev kumar <166992317+Devkumar2024@users.noreply.github.com>
2026-09-17 23:13:13 +0530

    Promise APIs

---
+++ Callback&promise/promiseRace.js
@@ -0,0 +1,31 @@
+// Promise.race()
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve(`p1 promise fulfilled`);
+  }, 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    reject(`p2 promise reject`);
+  }, 2500);
+//   setTimeout(function () {
+//     resolve(`p2 promise fulfilled`);
+//   }, 2500);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve(`p3 promise fulfilled`);
+  }, 1000);
+});
+
+// took 3 sec to get the result
+Promise.race([p1, p2, p3])
+.then((result) => console.log(result))
+.catch((error)=>{ console.error(error);
+    console.log(error.errors);
+});
+
+// p1,p2,p3 can be api calls also
